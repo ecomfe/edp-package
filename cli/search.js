2498d6bac1e226c7dda14236b08c8075e270824b
erik <erik168@163.com>
2014-03-22 20:47:08 +0800

    add cli 4 edp command, untested

---
+++ cli/search.js
@@ -0,0 +1,37 @@
+/**
+* @file 包查询功能
+* @author errorrik[errorrik@gmail.com]
+* fanxueliang[fanxueliang.g@gmail.com]
+*/
+/**
+* 命令行配置项
+*
+* @inner
+* @type {Object}
+*/
+var cli = {};
+
+/**
+* 命令描述信息
+*
+* @type {string}
+*/
+cli.description = '查询现有的包';
+
+/**
+* 模块命令行运行入口
+*
+* @param {Array} args 命令运行参数
+*/
+cli.main = function ( args ) {
+    var keyword = args[ 0 ] || '';
+    var search = require( '../lib/search' );
+    search( keyword, function ( error, data ) {} );
+};
+
+/**
+* 命令行配置项
+*
+* @type {Object}
+*/
+exports.cli = cli;
---
+++ cli/update.js
@@ -0,0 +1,47 @@
+/**
+* @file 项目更新依赖包模块的命令行执行
+* @author errorrik[errorrik@gmail.com]
+* Ielgnaw[wuji0223@gmail.com]
+*/
+
+
+/**
+* 命令行配置项
+*
+* @inner
+* @type {Object}
+*/
+var cli = {};
+
+/**
+* 命令描述信息
+*
+* @type {string}
+*/
+cli.description = '更新依赖包';
+
+/**
+* 命令选项信息
+*
+* @type {Array}
+*/
+cli.options = [ 'reserve-older', 'force' ];
+
+/**
+* 模块命令行运行入口
+*
+* @param {Array} args 命令运行参数
+* @param {Object} opts 命令运行选项
+*/
+cli.main = function ( args, opts ) {
+    var name = args[ 0 ];
+    var update = require( '../lib/update' );
+    update( name, process.cwd(), opts );
+};
+
+/**
+* 命令行配置项
+*
+* @type {Object}
+*/
+exports.cli = cli;
