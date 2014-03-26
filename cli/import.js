430d5dc6d718d0582d7fdd8dacd5665107922bae
erik <erik168@163.com>
2014-03-26 10:58:41 +0800

    comment code style

--- cli/import.js
+++ cli/import.js
@@ -1,36 +1,36 @@
 /**
-* @file 项目导入依赖包模块的命令行执行
-* @author errorrik[errorrik@gmail.com]
-*/
+ * @file 项目导入依赖包模块的命令行执行
+ * @author errorrik[errorrik@gmail.com]
+ */
 
 
 /**
-* 命令行配置项
-*
-* @inner
-* @type {Object}
-*/
+ * 命令行配置项
+ *
+ * @inner
+ * @type {Object}
+ */
 var cli = {};
 
 /**
-* 命令描述信息
-*
-* @type {string}
-*/
+ * 命令描述信息
+ *
+ * @type {string}
+ */
 cli.description = '导入包';
 
 /**
-* 命令选项信息
-*
-* @type {Array}
-*/
+ * 命令选项信息
+ *
+ * @type {Array}
+ */
 cli.options = [ 'older', 'save-dev' ];
 
 /**
-* 模块命令行运行入口
-*
-* @param {Array} args 命令运行参数
-*/
+ * 模块命令行运行入口
+ *
+ * @param {Array} args 命令运行参数
+ */
 cli.main = function ( args, opts ) {
     var name = args[ 0 ];
     if ( !name ) {
@@ -62,8 +62,8 @@ cli.main = function ( args, opts ) {
 };
 
 /**
-* 命令行配置项
-*
-* @type {Object}
-*/
+ * 命令行配置项
+ *
+ * @type {Object}
+ */
 exports.cli = cli;
