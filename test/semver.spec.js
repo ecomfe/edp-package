68f991b88bc53c6b18f2917c095ba07201fb7f0a
leeight <leeight@gmail.com>
2014-03-28 16:45:37 +0800

    Fix edp jshint issue
    ecomfe/edp#199

--- test/semver.spec.js
+++ test/semver.spec.js
@@ -1,17 +1,17 @@
 /***************************************************************************
- * 
+ *
  * Copyright (c) 2014 Baidu.com, Inc. All Rights Reserved
- * $Id$ 
- * 
+ * $Id$
+ *
  **************************************************************************/
- 
- 
- 
+
+
+
 /**
  * semver.spec.js ~ 2014/02/24 11:21:59
  * @author leeight(liyubei@baidu.com)
- * @version $Revision$ 
- * @description 
+ * @version $Revision$
+ * @description
  * 测试一下调用semver的的地方
  **/
 
@@ -21,11 +21,11 @@ describe('semver', function(){
     it('default', function(){
         // er的版本号
         var versions = [
-            "3.0.0", "3.0.1",
-            "3.0.2", "3.0.3",
-            "3.1.0-alpha.1","3.1.0-alpha.2",
-            "3.1.0-alpha.3","3.1.0-alpha.4",
-            "3.1.0-alpha.5","3.1.0-alpha.6"
+            '3.0.0', '3.0.1',
+            '3.0.2', '3.0.3',
+            '3.1.0-alpha.1','3.1.0-alpha.2',
+            '3.1.0-alpha.3','3.1.0-alpha.4',
+            '3.1.0-alpha.5','3.1.0-alpha.6'
         ];
 
         // 命令行中指定的版本号
